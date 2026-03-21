be4dd77bb35b1b559dde84311fc38473d7f58091
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2026-03-21 23:08:56 +0530

    Added some more frontend components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,15 @@ import {Toaster} from 'react-hot-toast'
 import ProtectedRoute from './components/protectedRoutes'
 import PublicRoute from './components/publicRoutes'
 import SelectRole from './pages/SelectRole'
+import Navbar from './components/navbar'
+import Account from './pages/Account'
 
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route element={<PublicRoute/>}>
             <Route path='/login' element={<Login/>}/>
@@ -20,6 +23,7 @@ const App = () => {
           <Route element={<ProtectedRoute/>}>
             <Route path='/' element={<Home/>}/>
             <Route path='/select-role' element={<SelectRole/>}/>
+            <Route path='/account' element={<Account/>}/>
 
           </Route>
         </Routes>
