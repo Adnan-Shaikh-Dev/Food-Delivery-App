746d2d7dadcc3298436424b1e44c1abb690eb3fd
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2026-03-23 20:03:03 +0530

    Added microservice utils for cloudinary and microservice restaurant for adding restaurant

---
+++ services/restaurant/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import restaurantRoutes from './routes/restaurant.js'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5001;
+
+app.use('/api/restaurant', restaurantRoutes)
+
+app.listen(PORT, ()=>{
+    console.log(`Restaurant service is running on port ${PORT}`)
+    connectDB()
+})
