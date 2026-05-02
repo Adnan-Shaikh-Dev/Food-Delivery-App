c5b63f8bde1613a4f942a50a89043bb74c80b4e2
Adnan-Shaikh-Dev <adnanshaikhcareer@gmail.com>
2026-05-02 17:26:26 +0530

    started working on integration of rabbitmq and also worked on payment routes

--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -1,36 +1,37 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cloudinary from 'cloudinary'
-import cors from 'cors'
-import uploadRoutes from './routes/cloudinary.js'
+import express from "express";
+import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+import cors from "cors";
+import uploadRoutes from "./routes/cloudinary.js";
+import paymentRoutes from "./routes/payments.js";
+import { connectRabbitMQ } from "./config/rabbitmq.js";
 
-
-dotenv.config()
+dotenv.config();
+connectRabbitMQ();
 
 const app = express();
 
+app.use(cors());
 
-app.use(cors())
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_SECRET_KEY } = process.env;
 
-app.use(express.json({limit:'50mb'}));
-app.use(express.urlencoded({limit:'50mb', extended:true}))
-const {CLOUD_NAME, CLOUD_API_KEY, CLOUD_SECRET_KEY} = process.env
-
-if(!CLOUD_NAME || !CLOUD_API_KEY || !CLOUD_SECRET_KEY){
-    throw new Error("Missing cloudinary environment variables")
+if (!CLOUD_NAME || !CLOUD_API_KEY || !CLOUD_SECRET_KEY) {
+  throw new Error("Missing cloudinary environment variables");
 }
 
 cloudinary.v2.config({
-    cloud_name:CLOUD_NAME,
-    api_key:CLOUD_API_KEY,
-    api_secret:CLOUD_SECRET_KEY
-})
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_SECRET_KEY,
+});
 
-app.use('/api',uploadRoutes)
+app.use("/api", uploadRoutes);
+app.use("/api/payment", paymentRoutes);
 
+const PORT = process.env.PORT || 5002;
 
-const PORT = process.env.PORT || 5002
-
-app.listen(PORT, ()=>{
-    console.log(`Utils Service is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Utils Service is running on port ${PORT}`);
+});
